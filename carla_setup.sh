3f7043bfe7536a9c3d6609679969c1694de38d09
caffcen <caffcen@outlook.com>
2026-02-02 15:18:50 -0500

    add carla_setup.sh

---
+++ carla_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export UE4_ROOT=~fmuelle/carla-9.14/UnrealEngine_4.26/
+export PYTHONPATH=~fmuelle/carla-9.14/PythonAPI/carla/dist/carla-0.9.14-py3.7-linux-x86_64.egg
+export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
